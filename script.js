0a4a609a3182eb302a46a12d2c9566f381a31c7a
adrian candia <eidanblack19@gmail.com>
2022-05-30 18:45:00 -0400

    almost finish

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ let card2 = null;
 let firstResult = null;
 let secondResult = null;
 let movements = null;
+let score = 0;
 
 // Apuntando al documento HTML
 let showMovements = document.getElementById('movimientos');
+let showScore = document.getElementById('aciertos');
+let allButtons = document.querySelectorAll('button');
+console.log(allButtons);
 
 // Generar números aleatorios en un array
 let numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
@@ -34,5 +38,30 @@ function showCard(id) {
         // Incrementar la cantidad de movimientos
         movements++;
         showMovements.innerHTML = `Movimientos: ${movements}`;
+
+        if(firstResult == secondResult) {
+            console.log("son iguales!")
+            score++;
+            showScore.innerHTML = `Aciertos: ${score}`;
+            if (score == 8) {
+                setTimeout(function(){
+                    allButtons.forEach(button =>{
+                        button.innerHTML = "";
+                        button.disabled = false;
+                    })
+                    movements = 0;
+                    score = 0;
+                    showMovements.innerHTML = "Movimientos: 0";
+                    showScore.innerHTML = "Aciertos: 0";
+                }, 700);
+            }
+        } else {
+            setTimeout(function(){
+                card1.innerHTML = "";
+                card2.innerHTML = "";
+                card1.disabled = false;
+                card2.disabled = false;
+            }, 700);
+        }
     }
 }
