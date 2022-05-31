1358938fda6a321143c754341630e8bc9f02799c
adrian candia <eidanblack19@gmail.com>
2022-05-30 22:36:13 -0400

    added tempo and reset button

--- script.js
+++ script.js
@@ -6,21 +6,49 @@ let firstResult = null;
 let secondResult = null;
 let movements = null;
 let score = 0;
+let timer = false;
+let countdown = 30;
+let countDownTimer = null;
+let initialTime = countdown;
 
 // Apuntando al documento HTML
 let showMovements = document.getElementById('movimientos');
 let showScore = document.getElementById('aciertos');
 let allButtons = document.querySelectorAll('button');
+let showTimer = document.getElementById('t-restante');
 
 // Generar números aleatorios en un array
 let numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 numbers = numbers.sort(()=> Math.random()-0.5);
 console.log(numbers);
 
+function countTime(){
+    countDownTimer = setInterval(()=>{
+        countdown--;
+        showTimer.innerHTML = `Tiempo: ${countdown} segundos`;
+        if(countdown == 0){
+            clearInterval(countDownTimer);
+            blockCards();
+            showTimer.innerHTML = "Fallaste 😓 inténtalo de nuevo!";
+        }
+    }, 1000);   
+}
+
+function blockCards(){
+    for(let i = 0; i <= 15; i++){
+        let blockedCard = document.getElementById(i);
+        blockedCard.innerHTML = numbers[i];
+        blockedCard.disabled = true;
+    }
+}
+
 // Función al hacer click en un botón
 function showCard(id) {
     showedCards++;
-    console.log(showedCards);
+    if(timer == false) {
+        countTime();
+        timer = true;
+    }
     if(showedCards == 1){
         // Mostrar el primer botón
         card1 = document.getElementById(id);
@@ -41,16 +69,22 @@ function showCard(id) {
             score++;
             showScore.innerHTML = `Aciertos: ${score}`;
             if (score == 8) {
-                setTimeout(function(){
-                    allButtons.forEach(button =>{
-                        button.innerHTML = "";
-                        button.disabled = false;
-                    });
-                    movements = 0;
-                    score = 0;
-                    showMovements.innerHTML = "Movimientos: 0";
-                    showScore.innerHTML = "Aciertos: 0";
-                }, 900);
+                showScore.innerHTML = `Aciertos: ${score} 🔥`;
+                showMovements.innerHTML = `Movimientos: ${movements} 😉🤘`;
+                showTimer.className += " minText";
+                showTimer.innerHTML = `Increible! 😃 Solo te demoraste: ${initialTime - countdown} segundos 😱`;
+                clearInterval(countDownTimer);
+                //setTimeout(function(){
+                    //allButtons.forEach(button =>{
+                    //    button.innerHTML = "";
+                   //     button.disabled = false;
+                   // });
+                   // movements = 0;
+                   // score = 0;
+                   // showTimer.innerHTML = "Tiempo: 30 segundos";
+                  //  showMovements.innerHTML = "Movimientos: 0";
+                  //  showScore.innerHTML = "Aciertos: 0";
+               // }, 900);
             }
         } else {
             setTimeout(function(){
@@ -64,3 +98,4 @@ function showCard(id) {
         
     }
 }
+
